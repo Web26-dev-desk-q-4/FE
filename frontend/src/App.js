8509de7ce2baa22dbf88d60a4d996f24599b39e7
debauchery1st <trevor.j.martin@gmail.com>
2020-01-06 17:30:45 -0500

    login, navbar placeholder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
 import LoginForm from "./components/loginForm";
+import NavBar from "./components/navBar";
+// import { b64encode, b64decode } from "./components/common/chillout/madmurphy";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [appState, setAppState] = useState({
+    account: { username: "", password: "" }
+  });
+  const endPointURL = "https://whatever.what";
+  const history = useHistory();
+  const loginFormSubmit = ({ data: account, errors }) => {
+    // we should send the account info to the backend
+    const clone = { ...appState };
+    clone.account = account;
+    axios.put(endPointURL, account).then(result => {
+      // do something
+      history.replace("/nextpage");
+    });
+    setAppState(clone);
+  };
+
+  const loginForm = () => {
+    return <LoginForm onSubmit={loginFormSubmit} />;
+  };
+
+  const registerForm = () => {
+    return <h1>TO DO (register form)</h1>;
+  };
+
+  const homePage = () => {
+    return <h1>TO DO (homepage)</h1>;
+  };
+
   return (
     <div className="App">
-      <LoginForm />
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={homePage} />
+        <Route path="/register" component={registerForm} />
+        <Route path="/login" component={loginForm} />
+      </Switch>
     </div>
   );
 }
