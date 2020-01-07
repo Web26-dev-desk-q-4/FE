58809843e15083900a7e6a53a9274076e8305e47
debauchery1st <trevor.j.martin@gmail.com>
2020-01-07 12:40:07 -0500

    cleanup + testcode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import Login2 from "./components/Login2";
-// import { b64encode, b64decode } from "./components/common/chillout/madmurphy";
+
 import PrivateRoute from "./components/PrivateRoute";
 import TicketViews from "./views/TicketViews";
 import Registration2 from "./components/Registration2";
@@ -12,12 +12,8 @@ import TicketCard from "./components/Tickets/TicketCard";
 import Edit from "./components/Tickets/Edit";
 import MyTickets from "./components/Tickets/MyTickets";
 
-// import React, { useState } from "react";
-// import { Route, Switch, useHistory } from "react-router-dom";
-// import LoginForm from "./components/loginForm";
-// import NavBar from "./components/navBar";
-// // import { b64encode, b64decode } from "./components/common/chillout/madmurphy";
-// import "./App.css";
+import TestFormSelect from "./components/testFormSelect"; // example form
+
 import axios from "axios";
 
 function App() {
@@ -50,6 +46,21 @@ function App() {
     return <Registration2 onSubmit={registrationFormSubmit} />;
   };
 
+  const testAnyComponent = () => {
+    return (
+      <TestFormSelect
+        onSubmit={e => console.log(e)}
+        priorities={[
+          "when you have some time...",
+          "please make some time for ...",
+          "don't, you ...forget about",
+          "OK Seriously... ",
+          "TOP PRIORITY!"
+        ]}
+      />
+    );
+  };
+
   return (
     <Router>
       <NavBar />
@@ -66,10 +77,11 @@ function App() {
         <PrivateRoute path="/edit/:id" component={Edit} />
         <PrivateRoute exact path="/tickets" component={TicketViews} />
         <Route path="/registration" component={Registration2} />
-
         <Route path="/" exact component={loginForm} />
         <Route path="/register" component={registerForm} />
         <Route path="/login" component={loginForm} />
+
+        <Route path="/test" component={testAnyComponent} />
       </Switch>
     </Router>
   );
